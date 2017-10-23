67d5fdda72cc05ab0c8be8a2d259bb5e627a50e3
Ramanathan, Anandi <anandi.ramanathan@accenture.com>
2017-10-23 22:05:22 +0530

    changes to campaign

---
+++ pareto-ui/app/component/campaign/campaign.ts
@@ -0,0 +1,64 @@
+import {Component} from '@angular/core';
+import {HttpService} from '../../service/http.service';
+
+
+@Component({
+    selector: 'campaignhtml',
+    templateUrl: 'app/component/campign/campaign.html',
+    providers: [HttpService]
+})
+
+export class CampaignComponent{
+
+	constructor( private httpService: HttpService ) {
+        
+    }
+    
+   //simple call init function on controller
+   
+    
+    
+    i=65;
+	step = 0;
+	
+    campaignData : any;
+ 
+    
+    public ngOnInit(): any
+    {
+        console.log("reached here");
+        this.getcampaignData();
+    }
+    
+    getcampaignData(){
+        
+        this.httpService.getcampaigns("campaignData").subscribe(
+        resp => {    
+            if(resp!=null){
+                
+                this.campaignData= resp.response;
+            }
+            console.log(typeof this.campaignData);
+            console.log(this.campaignData);
+        },
+        error => {
+            console.log(error);
+        }
+        );   
+
+    }
+
+    getCurrentStep() {
+        return this.step;
+    }
+
+    goback(){
+        this.step = this.step - 1;    }
+
+    toReport(){
+        this.step = this.step + 1;    }
+}
+
+
+
+
--- pareto-ui/app/module.ts
+++ pareto-ui/app/module.ts
@@ -3,11 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { AlertModule, TooltipModule, ModalModule } from 'ng2-bootstrap';
+import { RouterModule, Routes } from '@angular/router';
+import {Ng2PaginationModule} from 'ng2-pagination'
 import { AppComponent } from './app.component';
 import { PatientComponent } from './component/patient/patient';
-import { CampignComponent } from './component/campign/campign';
-import {Ng2PaginationModule} from 'ng2-pagination'
+import { CampaignComponent } from './component/campign/campaign';
 
+const appRoutes: Routes = [
+  { path: 'patient', component: PatientComponent },
+  { path: 'campaign', component: CampaignComponent },
+  {
+    path: '',
+    redirectTo: '/patient',
+    data:  'full'
+  },
+];
 
 @NgModule({
   imports: [BrowserModule, FormsModule, ReactiveFormsModule, Ng2PaginationModule, HttpModule, AlertModule.forRoot(), TooltipModule.forRoot(), ModalModule.forRoot()],
